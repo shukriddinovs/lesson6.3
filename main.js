1039e8864bc7aaaf0b6189a7b4fbae1c9b422370
shukriddinovs <sshukriddinov524@gmail.com>
2024-05-23 15:52:51 +0500

    {.}

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const person= {
+    Name: "John",
+    Age : 30,
+    Gender :"New York"
+}
+for(let key in person){
+console.log(person[key]);
+}
+// for ( kalit in person){
+//     console.log(kalit + ':'+ person[kalit]);
+// }
